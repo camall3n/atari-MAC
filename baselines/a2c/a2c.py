Fix computation of q_acted

baselines/a2c/a2c.py
@@ -124,6 +124,8 @@ class Runner(object):
         mb_states = self.states
         for n in range(self.nsteps):
             actions, values, qvalues, states = self.model.step(self.obs, self.states, self.dones)
+            # save only the acted Q-values
+            qvalues = qvalues[np.arange(len(qvalues)), actions]
             mb_obs.append(np.copy(self.obs))
             mb_actions.append(actions)
             mb_qvalues.append(qvalues)
@@ -175,7 +177,7 @@ class Runner(object):
             ep_dones = np.asarray(self.dones, dtype=np.bool)
 
             for n in range(int(maxStepsPerEpisode)):
-                actions, _, _ states = self.model.step(self.obs, self.states, self.dones)
+                actions, _, _, states = self.model.step(self.obs, self.states, self.dones)
                 step_dones.append(self.dones)
                 obs, rewards, dones, _ = self.env.step(actions)
                 self.states = states

baselines/a2c/policies.py
@@ -108,12 +108,11 @@ class CnnPolicy(object):
             v  = tf.reduce_sum(pi*q, axis=1)
 
         a0 = sample(pi_logits)
-        q_act = tf.gather(q, a0, axis=1)
         self.initial_state = [] #not stateful
 
         def step(ob, *_args, **_kwargs):
-            a, val, qval = sess.run([a0, v, q_act], {X:ob})
-            return a, val, qval, [] #dummy state
+            a, val, qvals = sess.run([a0, v, q], {X:ob})
+            return a, val, qvals, [] #dummy state
 
         def value(ob, *_args, **_kwargs):
             return sess.run(v, {X:ob})