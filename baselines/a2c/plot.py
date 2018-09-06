92953eac27c993bcb71f141785ae8cc9f9f91db1
Cam Allen <csal@brown.edu>
2018-09-06 00:05:34 -0400

    Clean up plot script and remove extra files

--- baselines/a2c/plot.py
+++ baselines/a2c/plot.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
-import fnmatch
 import os
 
 logdir = "logs/BreakoutNoFrameskip-v4/"
@@ -27,32 +26,14 @@ def read_file(filename, pattern="eval_avg_score", eval_interval=1.0):
 
 	return time, data
 
-# log_files = sorted(fnmatch.filter(os.listdir('.'), "*log*.txt"))
-# log_files = ["2017-11-02-17-31-06-331713-log.txt",
-# 			 "2017-11-02-19-55-40-032464-log.txt",
-# 			 "2017-11-05-11-57-30-812910-log-WIP.txt",
-# 			 "2017-11-05-12-05-20-545401-log-WIP.txt"]
 log_files = ["2017-11-06-14-36-11-435842/log.txt",
-			 "2017-11-06-14-36-40-634245/log.txt",
-			 "2017-11-06-14-37-10-051730/log.txt",
-			 "2017-11-06-14-37-53-921575/log.txt",
-			 "2017-11-06-14-38-38-830422/log.txt",
-			 "2017-11-06-14-39-56-109458/log.txt"]
-# labels = ['Step 1. Switch to Q function',
-# 		  'Step 2. Switch ADV from (Yn-V) to (Q-V)',
-# 		  'Step 2. (Q-V), parallel by 32',
-# 		  'Step 2. (Q-V), parallel by 32']
-labels = ['nsteps=1',
-		  'nsteps=2',
-		  'nsteps=3',
-		  'nsteps=4',
-		  'nsteps=5',
-		  'nsteps=8']
-eval_interval = [.2, .4, .6, .8, 1.0, 1.6]
+			 "2017-11-06-14-36-40-634245/log.txt"]
+labels = ['AAC',
+		  'MAC']
 
 def visualize(log_files, labels, metric):
-	for filename, label, interval in zip(log_files, labels, eval_interval):
-		time, data = read_file(filename, metric, interval)
+	for filename, label in zip(log_files, labels):
+		time, data = read_file(filename, metric)
 		if "explained" in metric:
 			data = np.maximum(data, -2)
 		plt.plot(time, data, label=label)
@@ -61,14 +42,6 @@ def visualize(log_files, labels, metric):
 	plt.legend()
 	plt.ylabel(metric)
 
-plt.subplot(221)
 visualize(log_files, labels, "eval_avg_score")
-plt.subplot(222)
-visualize(log_files, labels, "explained_variance")
-plt.subplot(223)
-visualize(log_files, labels, "value_loss")
-plt.subplot(224)
-visualize(log_files, labels, "entropy")
-
 plt.show()
 plt.close()
